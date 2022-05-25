2628a43b264619fb40bad23af97b5818fc39147f
Neander Marcel Heming <neanderh@yahoo.com.br>
2022-05-25 16:07:25 -0300

    spp_rich functionality
    - added support for matrix and SpatRast objects in spp_rich
    -  old vectorized "spp_rich" is now hidden, not exported. It is called by global spp_rich function

--- R/2-spp_rich.R
+++ R/2-spp_rich.R
@@ -4,16 +4,44 @@
 #' espécies de uma comunidade.
 #'
 #' @param x A numeric vector of species abundances/presences in the community
-#' @param ... arguments to be passed to internal functions
+#' @inheritParams base::sum
 #'
 #' @return numeric
-#' @export
+# #' @export
 #'
 #' @examples
 #' mata <- c(3, 7, 10, 2, 0, 1)
 #' spp_rich(mata)
 #'
-spp_rich <- function(x, ...){
-  r <- sum(x>0, ...)
+.spp_rich <- function(x, na.rm = FALSE){
+  r <- sum(x>0, na.rm = na.rm)
+  return(r)
+}
+
+
+
+#' Computes species richness
+#'
+#' @inheritParams .spp_rich
+#'
+#' @return An object of the same class of x (i.e. numeric vector, matrix or SpatRaster)
+#' @export
+#'
+#' @examples
+#' spp_rich(c(3, 7, 10, 2, 0, 1, NA))
+#'
+spp_rich <- function(x, na.rm = FALSE){
+  if(inherits(x, c("integer", "numeric"))){
+    ## vetor
+    r <- .spp_rich(x, na.rm=na.rm)
+  } else if(inherits(x, "matrix")){
+    ## matriz
+    r <- apply(x, 1, .spp_rich, na.rm=na.rm)
+  } else if(inherits(x, "SpatRaster")){
+    ## raster
+    r <- terra::app(x, .spp_rich, na.rm=na.rm)
+  } else {
+    stop("Argument 'x' must be an object of class 'numeric', 'matrix' or 'SpatRaster'")
+  }
   return(r)
 }
